af6053b454ba862c914705f89a137aa643b2ae61
Dmitry Fuchko <dmitii_fuchko@mail.ru>
2021-11-13 10:28:39 +0300

    create new branch

--- HomeWork_OOP.py
+++ HomeWork_OOP.py
@@ -59,7 +59,8 @@ class Lecturer(Mentor):
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) and course in self.courses_attached \
+                and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
